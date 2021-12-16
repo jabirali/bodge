2484515915706e7f8547d170435821e22ff53811
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2021-12-16 21:02:35 +0100

    add: initial hamiltonian construction

--- hamiltonian.py
+++ hamiltonian.py
@@ -1,6 +1,16 @@
 import numpy as np
 import functools as ft
 
+from scipy.sparse import lil_matrix
+
+σ = [*map(lambda s: np.array(s, dtype=np.complex64),
+    (
+        [[1, 0], [0, 1]],
+        [[0, 1], [1, 0]],
+        [[0, -1j], [1j, 0]],
+        [[1, 0], [0, -1]]
+    ))]
+
 class Hamiltonian(object):
     """TODO"""
     def __init__(self, size):
@@ -16,11 +26,40 @@ class Hamiltonian(object):
         self.t = np.zeros((N, N))  # Hopping amplitudes
 
         # Hamiltonian.
-        self.H = np.zeros((4*N, 4*N), dtype=np.float32)
+        # self.H = lil_matrix((4*N, 4*N), dtype=np.complex64)
+        self.H = np.zeros((4*N, 4*N), dtype=np.complex64)
 
     def asmatrix(self):
         """Construct a matrix representation."""
+        N = self.H.shape[0] // 4
+
+        # Handle the diagonal terms.
+        for i in range(N):
+            # Calculate the electronic contribution.
+            H_ii = -(
+                self.μ[i, 0] * σ[0] +
+                self.m[i, 0] * σ[1] +
+                self.m[i, 1] * σ[2] +
+                self.m[i, 2] * σ[3]
+            )
+
+            Δ_ii = self.Δ[i, 0] * (1j*σ[2])
+
+            # Nambu⊗Spin matrix for these lattice sites.
+            self.H[4*i+0:4*i+2, 4*i+0:4*i+2] = +H_ii
+            self.H[4*i+2:4*i+4, 4*i+2:4*i+4] = -H_ii.conj()
+            self.H[4*i+0:4*i+2, 4*i+2:4*i+4] = +Δ_ii
+            self.H[4*i+2:4*i+4, 4*i+0:4*i+2] = +Δ_ii.T.conj()
+
+        # TODO: Handle hopping.
+
         return self.H
 
-H = Hamiltonian([2,3])
-print(H.asmatrix())
+G = Hamiltonian([1,2])
+
+G.μ[:, 0] = 1
+G.m[:, 1] = 2
+G.Δ[:, 0] = 3
+
+with np.printoptions(precision=0, suppress=True):
+    print(G.asmatrix())
