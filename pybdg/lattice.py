c6a6f20f3a62b35f572672cf4c6ed5048641fddf
Jabir Ali Ouassou <jabirali@hobina.home>
2022-01-04 18:06:29 +0100

    feat: lattice class for simplifying indexing

---
+++ pybdg/lattice.py
@@ -0,0 +1,58 @@
+class Lattice:
+	"""Representation of a cubic lattice.
+
+	This class provides convenience methods for iterating over all sites
+	or neighbors in a lattice, as well as methods for converting between
+	index (i1, i2) and coordinate ((x1, y1, z1), (x2, y2, z2)) notations.
+	This simplfies the process of preparing lattice Hamiltonian matrices
+	using flat indices while retaining the convenience of coordinates.
+	"""
+
+	def __init__(self, dims):
+		self.dims = dims
+
+	def __getitem__(self, inds):
+		"""Convert between coordinate and index notations."""
+		return inds[2] + inds[1]*self.dims[2] + inds[0]*self.dims[1]*self.dims[2]
+
+	def sites(self):
+		"""Generator for site coordinates in the lattice."""
+		for x in range(self.dims[0]):
+			for y in range(self.dims[1]):
+				for z in range(self.dims[2]):
+					yield (x, y, z)
+
+	def neighbors(self, axis=None):
+		"""Generator for nearest-neighbor coordinates in the lattice.
+
+		The argument `axis` specifies whether we are only interested in
+		nearest-neighbor pairs along a certain direction, which is useful
+		when e.g. specifying hopping terms for spin-orbit coupling.
+
+		Note that this returns pairs only in the "increasing" direction.
+		For instance, it yields ((0,0,0), (0,0,1)) but ((0,0,1), (0,0,0)),
+		such that any inverse hopping terms must be dealt with yourself.
+		"""
+		if axis is None:
+			# Neighbors along all axes.
+			yield from self.neighbors(axis=2)
+			yield from self.neighbors(axis=1)
+			yield from self.neighbors(axis=0)
+		elif axis == 0:
+			# Neighbors along x-axis.
+			for x in range(self.dims[0]-1):
+				for y in range(self.dims[1]):
+					for z in range(self.dims[2]):
+						yield (x, y, z), (x+1, y, z)
+		elif axis == 1:
+			# Neighbors along y-axis.
+			for x in range(self.dims[0]):
+				for y in range(self.dims[1]-1):
+					for z in range(self.dims[2]):
+						yield (x, y, z), (x, y+1, z)
+		elif axis == 2:
+			# Neighbors along z-axis.
+			for x in range(self.dims[0]):
+				for y in range(self.dims[1]):
+					for z in range(self.dims[2]-1):
+						yield (x, y, z), (x, y, z+1)
---
+++ tests/test_lattice.py
@@ -0,0 +1,31 @@
+from pybdg.lattice import *
+
+class TestLattice:
+    def test_sites(self):
+        lat = Lattice((3, 5, 7))
+        for ind, site in enumerate(lat.sites()):
+            # Verify that indexing is contiguous.
+            assert ind == lat[site]
+
+            # Verify that index bounds are satisfied.
+            assert site[0] >= 0 and site[0] < lat.dims[0]
+            assert site[1] >= 0 and site[1] < lat.dims[1]
+            assert site[2] >= 0 and site[2] < lat.dims[2]
+
+        # Verify that number of elements is correct.
+        assert ind == 3*5*7 - 1
+
+    def test_neighbors(self):
+        lat = Lattice((2, 3, 5))
+        for (x1,y1,z1), (x2,y2,z2) in lat.neighbors(axis=0):
+            # Verify neighbors along the x-axis.
+            assert x2 == x1 + 1 and y2 == y1 and z2 == z1
+        for (x1,y1,z1), (x2,y2,z2) in lat.neighbors(axis=1):
+            # Verify neighbors along the y-axis.
+            assert x2 == x1 and y2 == y1 + 1 and z2 == z1
+        for (x1,y1,z1), (x2,y2,z2) in lat.neighbors(axis=2):
+            # Verify neighbors along the z-axis.
+            assert x2 == x1 and y2 == y1 and z2 == z1 + 1
+
+        # Verify that the number of neighbors is correct.
+        len([*lat.neighbors()]) == 2*3*5 - 2*3 - 3*5 - 5*2
