3c3618703a9d03906ca1e727ed1cbceeb2275bc1
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-01-05 21:38:34 +0100

    wip: plotting of eigenwavefunctions

--- minimal.py
+++ minimal.py
@@ -3,6 +3,7 @@
 # Minimal validation example based on Sec. 2.6 of the Kwant documentation.
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 from scipy.linalg import eigh
 
@@ -20,7 +21,7 @@ Rpos = (3, 4)
 t = 1.0
 
 # Construct a system with the square lattice.
-lat = Lattice((W, L, 10))
+lat = Lattice((W, L, 1))
 sys = System(lat)
 
 for x, y, z in lat.sites():
@@ -43,30 +44,11 @@ H = sys.asarray()
 
 E, X = eigh(H, driver='evr', subset_by_value=(0, np.inf))
 χ = X.T.reshape((E.size, -1, 2, 2))  # Indices: n, i, eh, ↑↓
-print(χ.shape)
 
-# print(E.shape)  # 4000/2 positive eigenvalues
-# # print(X.shape)
-# # print(E)
-# χ = [X[:,n] for n, E_n in enumerate(E) if E_n > 0]
-# print(len(χ))  # 4000/2 corresponding eigenvectors
+# print(np.sum(np.abs(χ[1,:,1,0])))
+X = np.zeros((W, L))
+for x, y, z in lat.sites():
+	X[x, y] += np.abs(χ[:, lat[x, y, z], 0, :]).sum(axis=0).sum(axis=-1)
 
-# print(χ[0].shape)  # 2 particles * 2 spins * 1000 sites 
-
-# u_up = np.array([χ_n[0] for χ_n in χ])
-# u_dn = np.array([χ_n[1] for χ_n in χ])
-# v_up = np.array([χ_n[2] for χ_n in χ])
-# v_dn = np.array([χ_n[3] for χ_n in χ])
-
-# print(len(u_up))
-# # print(χ[0].shape	)
-
-# # print(χ[100])
-# # print(len(χ[100]))
-
-# # u_up = [χ_n[0] for χ_n in χ]
-# # print(u_up)
-
-# # E = [E_n for E_n in E if E_n > 0]
-# # print(E)
-# # # print(χ)
+plt.imshow(X.T)
+plt.show()
