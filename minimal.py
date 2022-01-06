fix: minor scaling and hermiticity issues

minimal.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from scipy.linalg import eigh
 
@@ -34,13 +35,13 @@ for x, y, z in lat.sites():
 
 	# Barrier is located between Rpos coordinates.
 	if x >= Rpos[0] and x < Rpos[1]:
-		sys.hopp[x, y, z] += R
+		sys.hopp[x, y, z] += R * σ0
 
 for r1, r2 in lat.neighbors():
 	sys.hopp[r1, r2] = -t * σ0
 
 # Obtain the Hamiltonian.
-H = sys.asarray()
+H = sys.hamiltonian()
 
 E, X = eigh(H, driver='evr', subset_by_value=(0, np.inf))
 χ = X.T.reshape((E.size, -1, 2, 2))  # Indices: n, i, eh, ↑↓
@@ -48,7 +49,9 @@ E, X = eigh(H, driver='evr', subset_by_value=(0, np.inf))
 # print(np.sum(np.abs(χ[1,:,1,0])))
 X = np.zeros((W, L))
 for x, y, z in lat.sites():
-	X[x, y] += np.abs(χ[:, lat[x, y, z], 0, :]).sum(axis=0).sum(axis=-1)
+	X[x, y] += np.abs(χ[:, lat[x, y, z], 0, 0].sum())
 
-plt.imshow(X.T)
+print(E)
+# plt.imshow(X.T)
+sns.heatmap(X.T, vmin=0, vmax=1)
 plt.show()

pybdg/system.py
@@ -57,12 +57,12 @@ class System:
 
 		# TODO: Create separate places to store the physical fields.
 
-	def asarray(self, copy=False):
+	def hamiltonian(self):
 		"""Represent the Hamiltonian of the system as a dense array.
 
 		This method also ensures that the matrix is finalized, i.e. that e.g.
 		particle-hole and hopping symmetries are ensured. One should always
-		explicitly call `.asarray()` to access Hamiltonian matrix, as using
+		explicitly call this method to access Hamiltonian matrix, as using
 		the `.data` field directly may leave matrix elements out-of-date.
 		"""
 		for r_i, r_j in self.lattice.relevant():
@@ -85,7 +85,11 @@ class System:
 			self.data[4*j+0 : 4*j+4, 4*i+0 : 4*i+4] = \
 				+self.data[4*i+0 : 4*i+4, 4*j+0 : 4*j+4].T.conj()
 
-		if not copy:
-			return self.data
-		else:
-			return self.data.copy()
+		# Scaling due to adding particle-hole symmetry.
+		self.data /= 2
+
+		# Verify that the matrix is Hermitian.
+		if not np.allclose(self.data, self.data.T.conj()):
+			raise RuntimeError("Error: Hamiltonian is not Hermitian!")
+
+		return self.data

tests/test_system.py
@@ -30,7 +30,7 @@ class TestSystem:
 			sys.hopp[r1, r2][:, :] = [[+3, +4j], [-4j, +3]]  # 3σ₀ - 4σ₂
 			sys.pair[r1, r2][:, :] = [[+2, -5j], [+5j, -2]]  # 2σ₃ + 5σ₂
 		
-		mat = sys.asarray()
+		mat = sys.hamiltonian()
 
 		# Verify that the result is Hermitian.
 		assert np.allclose(mat, mat.T.conj())