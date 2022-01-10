865f01789acec6bba00b2a705dca01e347e5e493
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-01-10 15:12:36 +0100

    fix: restored eigenvalue version

--- validate_kwant.py
+++ validate_kwant.py
@@ -22,63 +22,55 @@ W = 10
 L = 10
 R = 1.5
 Rpos = (3, 4)
-μ = 0.4
-Δ = 2
-Δpos = 6
+μ = 1
+Δ = 0.5
 t = 1.0
+m = 0.2
 
 # Construct a system with the square lattice.
 lat = Lattice((20, 20, 1))
 sys = System(lat)
 
-for x, y, z in lat.sites():
-	# At every site, we add a diagonal term 4t-μ.
-	sys.hopp[x, y, z][:, :] += -1 * σ0
+for i in lat.sites():
+	sys.hopp[i][...] -= μ*σ0 + m*σ3
+	sys.pair[i][...] += Δ*jσ2
 
-	# Superconductivity is included only for x>Δpos.
-	# if x > Δpos:
-	# m = 0.1
-	sys.hopp[x, y, z] += 0.2 * σ1
-	sys.pair[x, y, z][:, :] += 0.5 * jσ2
+for i, j in lat.neighbors():
+	sys.hopp[i, j][...] -= t*σ0
 
-	# Barrier is located between Rpos coordinates.
-	# if x >= Rpos[0] and x < Rpos[1]:
-		# sys.hopp[x, y, z] += R * σ0
-
-for r1, r2 in lat.neighbors():
-	sys.hopp[r1, r2][:, :] = -t * σ0
 
 # Obtain the Hamiltonian.
-H = bsr_matrix(sys.finalize(), blocksize=(4,4))
-I = bsr_matrix(np.identity(H.shape[0]))
+# H = bsr_matrix(sys.finalize(), blocksize=(4,4))
+# I = bsr_matrix(np.identity(H.shape[0]))
 
-ε = np.arange(0, 3, 0.1)
-i = 3
-N = np.zeros_like(ε)
-for n, ε_n in tqdm(enumerate(ε)):
-	G = (1j/np.pi) * inv((ε_n + 0.00001j) * I - H)
-	N[n] = np.real(G[4*i, 4*i] + G[4*i+1, 4*i+1])/2
+# ε = np.arange(0, 3, 0.1)
+# i = 3
+# N = np.zeros_like(ε)
+# for n, ε_n in tqdm(enumerate(ε)):
+# 	G = (1j/np.pi) * inv((ε_n + 0.00001j) * I - H)
+# 	N[n] = np.real(G[4*i, 4*i] + G[4*i+1, 4*i+1])/2
 
-plt.plot(ε, N)
-plt.show()
-# sys.diagonalize()
-
-# E, χ = sys.eigval, sys.eigvec
-
-# def delta(x):
-# 	w = E.max()/100
-# 	return np.exp(-x**2/(2*w**2)) / (w*np.sqrt(2*np.pi))
-
-# newval = np.arange(-E.max(), E.max(), E.max()/100)
-# dos = np.zeros_like(newval)
-# for n, E_n in enumerate(sys.eigval):
-# 	X = (np.abs(χ[n, :, :, :])**2).sum(axis=-1).sum(axis=-1).mean() / 2
-# 	for m, E_m in enumerate(newval):
-# 		dos[m] += (delta(E_n - E_m) + delta(E_n + E_m)) * X
-
-# plt.plot(newval, dos)
-# # sns.heatmap(dos, vmin=0, vmax=2)
+# plt.plot(ε, N)
 # plt.show()
+
+sys.finalize()
+sys.diagonalize()
+E, χ = sys.eigval, sys.eigvec
+
+def delta(x):
+	w = E.max()/100
+	return np.exp(-x**2/(2*w**2)) / (w*np.sqrt(2*np.pi))
+
+newval = np.arange(-E.max(), E.max(), E.max()/100)
+dos = np.zeros_like(newval)
+for n, E_n in enumerate(sys.eigval):
+	X = (np.abs(χ[n, :, :, :])**2).sum(axis=-1).sum(axis=-1).mean() / 2
+	for m, E_m in enumerate(newval):
+		dos[m] += (delta(E_n - E_m) + delta(E_n + E_m)) * X
+
+plt.plot(newval, dos)
+# # sns.heatmap(dos, vmin=0, vmax=2)
+plt.show()
 # N = 1000
 # dos = np.zeros((N, 10))
 # e = np.arange(0, N)/100
