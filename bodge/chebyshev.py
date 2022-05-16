723788b74b75f772475e112e4b434330c0e3e35d
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-05-16 16:06:35 +0200

    feat: initial working chebyshev solution (wip)

--- bodge/chebyshev.py
+++ bodge/chebyshev.py
@@ -10,19 +10,20 @@ class chebyshev(Kernel):
         N = self.energies // 2
         k = np.arange(2 * N)
         ω = np.cos(π * (2 * k + 1) / (4 * N))
-        w = N * π * np.sqrt(1 - ω**2)
+        w = π * np.sqrt(1 - ω**2)
 
         # Calculate the corresponding Chebyshev transform coefficients.
         # TODO: Incorporate the relevant Lorentz kernel factors here.
-        n = np.arange(N)
-        T = np.cos(n[None, :] * np.arccos(ω[:, None])) / N
-        T[:, 1:] *= 2
+        n = np.arange(N)[None, :]
+        k = np.arange(2 * N)[:, None]
+        T = np.cos(π * n * (2 * k + 1) / (4 * N))
+        # T[:, 1:] *= 2
 
         # Compact notation for the essential matrices.
         H = self.hamiltonian
         I_k = self.block_identity
         P_k = self.block_neighbors
-        R_k = self.block_subspace
+        R_k = self.block_subspace * 2
 
         # Initialize the first two Chebyshev matrix blocks A_kn, and calculate
         # the corresponding contributions to the spectral function A_k(ω_m).
@@ -38,14 +39,16 @@ class chebyshev(Kernel):
         for n in range(2, N):
             # Chebyshev expansion of next vector. Element-wise multiplication
             # by R_k projects the result back into the Local Krylov subspace.
-            A_k1, A_k0 = (H @ A_k1).multiply(R_k) - A_k0, A_k1
+            A_k1, A_k0 = 2 * (H @ A_k1) - A_k0, A_k1
+            # A_k1, A_k0 = (H @ A_k1).multiply(R_k) - A_k0, A_k1
 
             # Perform the Chebyshev transformation. Element-wise multiplication
             # by H_k preserves only on-site and nearest-neighbor interactions.
             # WARNING: This has been optimized to ignore SciPy wrapper checks.
-            AH_kn = A_k1.multiply(P_k)
+            # AH_kn = A_k1.multiply(P_k)
             for m, A_km in A_k.items():
-                A_k[m].data += T[m, n] * AH_kn.data
+                A_k[m] += 2 * T[m, n] * A_k1 / w[m]
+                # A_k[m].data += T[m, n] * AH_kn.data
 
         # Calculate the integral from unweighted results.
         with File(self.blockname, "w") as file:
@@ -54,8 +57,8 @@ class chebyshev(Kernel):
 
             if self.resolve:
                 # Scale the final results using the integral weights.
-                for m, A_km in A_k.items():
-                    A_km.data /= w[m]
+                # for m, A_km in A_k.items():
+                #     A_km.data /= w[m]
 
                 # Save energy-resolved information.
                 pack(file, f"/weights", w)
--- validate.py
+++ validate.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 
+import numpy as np
+from matplotlib.pyplot import plot, show, xlim
 
 from bodge import *
 
-t = 1.0
-μ = +3 * t
-Δ0 = t / 2
-m3 = t / 5
+t = 1
+Δ0 = t / 3
+μ = 0
+m3 = Δ0 / 2
 
 if __name__ == "__main__":
-    lattice = CubicLattice((128, 128, 64))
+    lattice = CubicLattice((4, 4, 4))
     hamiltonian = Hamiltonian(lattice)
-    solver = Solver(chebyshev, hamiltonian, resolve=False)
+    solver = Solver(chebyshev, hamiltonian, blocksize=32, energies=256, resolve=True)
 
     with hamiltonian as (H, Δ):
         for i in lattice.sites():
@@ -22,7 +24,38 @@ if __name__ == "__main__":
             H[i, j] = -t * σ0
 
     sol = solver()
-    print(sol.integral)
+    # print(sol.integral)
 
-    # for ω, A in sol.spectral:
-    # print(ω, A)
+    ws = []
+    D1 = []
+    x = lattice[1, 1, 1]
+    for ω, A in sol.spectral():
+        # x = sol.lattice[1, 1, 1]
+        dof = A.blocksize[0]
+        A_ii = A.diagonal()
+        A_up = A_ii[0 + x * dof]
+        A_dn = A_ii[1 + x * dof]
+        # print(len(A_ii), len(A_up), len(A_dn))
+        D1.append(A_up)
+        ws.append(ω)
+        # D = A[x]
+        # print(ω, D)
+
+    ws = [w * hamiltonian.scale for w in ws]
+    D1 = [d1 / hamiltonian.scale for d1 in D1]
+
+    D2 = []
+    # print(hamiltonian.scale)
+    A2 = hamiltonian.spectralize(ws, resolution=1e-2)
+    for A in A2:
+        A_ii = A.diagonal()
+        A_ii = A_ii
+        A_up = A_ii[0 + x * dof]
+        A_dn = A_ii[1 + x * dof]
+        D2.append(A_up)
+
+    plot(ws, D1, "b", ws, D2, "r")
+    show()
+
+# for ω, A in sol.spectral:
+# print(ω, A)
