a27028351e42c34f0bf40cace50fa73884f802c1
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-05-21 16:03:29 +0200

    add: better validation

--- plot_cutoff.py
+++ plot_cutoff.py
@@ -9,9 +9,24 @@ import sys
 
 # Load the output data.
 df = pd.read_csv(sys.argv[1])
-energies = df["Energies"].unique()[:-1]
+energies = df["Energies"].unique()
 
 # Plot the worst-case accuracy.
+fig, ax = plt.subplots()
+ax.set_xscale('log', base=2)
+ax.set_yscale('log', base=10)
+for energy in energies:
+	dfe = df[df["Energies"] == energy]
+	ax.plot(dfe["Radius"], dfe["Error"])
+
+plt.xlim([2, 512])
+plt.ylim([1e-8, 1])
+plt.legend([rf"$2^{{{int( np.log2(energy) )}}}$" for energy in energies], title="Energies", loc="lower left")
+plt.xlabel("Cutoff Radius $R$")
+plt.ylabel("DOS Max Absolute Error")
+plt.show()
+
+# Plot the average accuracy.
 fig, ax = plt.subplots()
 ax.set_xscale('log', base=2)
 ax.set_yscale('log', base=10)
--- validate_cutoff.py
+++ validate_cutoff.py
@@ -23,14 +23,14 @@ t = 1
 m3 = 0.25
 
 # Numerical parameters.
-energies = [32, 64, 128, 256, 512, 1024]
-radiuses = [1, 2, 4, 8, 16, 24, 32, 48, 64, 128, 256, 512, 1024]
+energies = [128, 256]
+radiuses = [1, 2, 4, 8, 16, 24, 32]
 interval = np.linspace(-4, 4, 1024)
 
 # Perform the validation.
 if __name__ == "__main__":
     # Construct a 1D test system.
-    lattice = CubicLattice((2048, 1, 1))
+    lattice = CubicLattice((32, 32, 32))
     hamiltonian = Hamiltonian(lattice)
     with hamiltonian as (H, Δ):
         for i in lattice.sites():
@@ -41,18 +41,20 @@ if __name__ == "__main__":
             H[i, j] = -t * σ0
 
     # Correct result.
-    x = lattice[512, 0, 0]
-    solver = Solver(chebyshev, hamiltonian, blocksize=64, energies=max(energies), radius=max(radiuses), resolve=True)
+    x = lattice[1, 1, 0]
+    solver = Solver(chebyshev, hamiltonian, blocksize=16, energies=max(energies), radius=max(radiuses), resolve=True)
     ωs, ds = solver().density()
     exact = spi.pchip(ωs[::-1], ds[x, ::-1])(interval)
-    def error(result):
+    def overlap(result):
         r1 = np.abs(result) / norm(result, 2)
         r2 = np.abs(exact) / norm(exact, 2)
 
         return np.dot(r1, r2)
+    def error(result):
+        return np.max(np.abs(result - exact))
 
     # Perform simulations.
-    print("Energies,Radius,Time,Overlap")
+    print("Energies,Radius,Time,Overlap,Error")
     for energy in energies:
         for radius in radiuses:
             # Instantiate solver.
@@ -67,4 +69,4 @@ if __name__ == "__main__":
             result = spi.pchip(ωs[::-1], ds[x, ::-1])(interval)
 
             # Print out the estimated error.
-            print(f"{energy},{radius},{sec},{error(result)}")
+            print(f"{energy},{radius},{sec},{overlap(result)},{error(result)}")
