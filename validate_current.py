0bf2e804cb423cd4eb72f461a727f8fcd0f54d81
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-05-25 12:50:36 +0200

    wip: testing current conservation

--- validate_current.py
+++ validate_current.py
@@ -2,7 +2,6 @@
 
 """Validation script to test current calculations."""
 
-import matplotlib.pyplot as plt
 import numpy as np
 
 from bodge import *
@@ -12,16 +11,27 @@ t = 1
 Δ0 = 1
 μ = 0.5
 m3 = 0.0
-d = 32
+d = 16
 
 # Numerical parameters.
-params = [(200, None), (400, None), (800, None), (1200, None)]
+params = [
+    (200, None),
+    (400, None),
+    (600, None),
+    (800, None),
+    (1000, None),
+    (1200, None),
+    (1400, None),
+    (1600, None),
+    (1800, None),
+    (2000, None),
+]
 phases = [π / 2]
 
 # Perform the validation.
 if __name__ == "__main__":
     # Construct a 1D test system.
-    lattice = CubicLattice((128, 1, 1))
+    lattice = CubicLattice((48, 16, 1))
     hamiltonian = Hamiltonian(lattice)
     with hamiltonian as (H, Δ):
         for i in lattice.sites():
@@ -30,14 +40,17 @@ if __name__ == "__main__":
         for i, j in lattice.bonds():
             H[i, j] = -t * σ0
 
+    # Define interlayer.
+    x1 = (lattice.shape[0] - d) // 2
+    x2 = (lattice.shape[0] + d) // 2
+
     # Perform simulations.
-    x = (lattice.shape[0] // 2, lattice.shape[1] // 2, 0)
     for energy, radius in params:
         # Instantiate solver.
         solver = Solver(
             chebyshev,
             hamiltonian,
-            blocksize=lattice.shape[0] // 8,
+            blocksize=lattice.shape[0] // 3,
             energies=energy,
             radius=radius,
             resolve=False,
@@ -47,25 +60,35 @@ if __name__ == "__main__":
         for φ in phases:
             with hamiltonian as (H, Δ):
                 for i in lattice.sites():
-                    if i[0] < lattice.shape[0] // 2 - d // 2:
+                    if i[0] < x1:
                         Δ[i, i] = (Δ0 * np.exp(-1j * φ / 2)) * jσ2
-                    if i[0] > lattice.shape[0] // 2 + d // 2:
+                    if i[0] > x2:
                         Δ[i, i] = (Δ0 * np.exp(+1j * φ / 2)) * jσ2
 
             results = solver().integral()
 
-            J = 0
+            J = {}
             for i, j in lattice.bonds(axis=0):
-                if i == x:
-                    k = hamiltonian.index(i, j)
-                    # - for electron, - included in t
-                    J += (
-                        2
-                        * hamiltonian.scale
-                        * np.imag(
-                            +hamiltonian.data[k, 0, 0] * results.data[k, 0, 0]
-                            + hamiltonian.data[k, 1, 1] * results.data[k, 1, 1]
-                        )
+                k = hamiltonian.index(i, j)
+                # - for electron, - included in t
+                Js = (
+                    2
+                    * hamiltonian.scale
+                    * np.imag(
+                        +hamiltonian.data[k, 0, 0] * results.data[k, 0, 0]
+                        + hamiltonian.data[k, 1, 1] * results.data[k, 1, 1]
                     )
+                )
+                if i[0] > x1 and i[0] < x2:
+                    try:
+                        J[i[0]] += Js
+                    except:
+                        J[i[0]] = Js
 
-            print(energy, φ, J)
+            J = np.array([Js for Js in J.values()])
+            diff = np.abs(J.max() - J.min())
+            mean = np.abs(np.mean(J))
+            tot = np.sum(J)
+            print(energy, φ, diff / (mean + 1e-100))
+
+            # print(energy, φ, J)
