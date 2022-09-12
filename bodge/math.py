f37932cf97bd05ef25ce59d52512c02ca32a5823
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-09-12 15:01:08 +0200

    add: local krylov expansion

--- bodge/math.py
+++ bodge/math.py
@@ -18,27 +18,51 @@ jσ2 = 1j * σ2
 jσ3 = 1j * σ3
 
 
-def chebyshev(X, I, N):
-    """Iterator that calculates the Chebyshev matrix polynomials T_n(X).
+def chebyshev(X, I, N=1024, R=None):
+    """Calculate the Chebyshev matrix polynomials T_n(X) for 0 ≤ n < N.
 
-    If the matrix X has dimensions M×M and I is a corresponding identity
-    matrix, we generate Chebyshev polynomials T_n(X) of order n ∈ [0, N].
-    The matrix X can be either sparse or dense, both are supported.
+    The arguments X and I should be square matrices with the same dimensions,
+    and these matrices can be either dense `np.array` or sparse `scipy.sparse`.
 
-    Alternatively, you can divide the full identity matrix into blocks
-    I = [I_1 ... I_K], in which case `chebyshev(X, I_k, N)` yields the
-    corresponding blocks T_nk(X) of the Chebyshev polynomials T_n(X).
-    This is useful to compute the matrix blocks in parallel.
+    Alternatively, you can divide the identity matrix I = [I_1, ..., I_K] into
+    K columns and provide only one such block I_k as an argument to this function.
+    This function will then calculate the corresponding blocks [T_n(X)]_k of the
+    Chebyshev polynomials, which is useful for parallel construction of T_n(X).
+
+    If the optional cutoff radius R is specified, the T_n(X) are projected
+    onto the Local Krylov subspace spanned by {X^0, ..., X^R}, so that the
+    T_n(X) for n > R are prevented from growing denser than T_R(X). This
+    argument of course only makes sense when using sparse matrices.
     """
+
     # T_0(X) is simply equal to the provided identity matrix.
     T_0 = I
     yield T_0
 
-    # T_1(X) reduces to X if I is the full identity matrix.
+    # T_1(X) reduces to X iff I is the full identity matrix.
     T_1 = X @ I
     yield T_1
 
-    # T_n(X) is calculated via a recursion relation.
+    # T_n(X) is calculated via the Chebyshev recursion relation.
     for n in range(2, N):
-        T_1, T_0 = 2 * X @ T_1 - T_0, T_1
+        T_1, T_0 = 2 * (X @ T_1) - T_0, T_1
+
+        # Local Krylov projection if a cutoff radius is specified.
+        if R is not None:
+            try:
+                # Construct a Local Krylov subspace mask from the sparsity
+                # structure of T_R(X), since this matrix contains all the
+                # relevant contributions {X^0, ..., X^R} of the subspace.
+                if n == R:
+                    P = T_1.astype(dtype="int8", copy=False)
+                    P.data[...] = 1
+
+                # Project T_n(x) onto the Local Krylov subspace spanned by
+                # elementwise multiplication by the mask constructed above.
+                elif n > R:
+                    T_1 = T_1.multiply(P)
+
+            except AttributeError:
+                raise ValueError("Cutoff radius is only supported for `scipy.sparse` matrices!")
+
         yield T_1
