5eb6a73cbfd9f2e8ed287c82928bca2e8e9fe48b
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-10-24 21:24:37 +0200

    clean: dvec implementation

--- pwave.py
+++ pwave.py
@@ -17,20 +17,17 @@ from bodge.typing import Coord
 # - Plot eigenvectors with eigenvalues below some threshold with different colors
 # Alternatively, use Nagai's Ax=b solving of resolvent operator with sparse A.
 
-Lx = 40
-Ly = 24
+Lx = 20
+Ly = 20
 
 t = 1
 μ = 0.1
-U = 1.5
 
 T = 1e-6 * t
 
 lattice = CubicLattice((Lx, Ly, 1))
 system = Hamiltonian(lattice)
-fermi = FermiMatrix(system, 2000)
-
-D = np.zeros((Lx,Ly,3,3))
+# fermi = FermiMatrix(system, 2000)
 
 def dvector(desc: str):
     """Convert a d-vector expression into a p-wave gap function."""
@@ -55,18 +52,15 @@ def dvector(desc: str):
     # Convert the d-vector expression to a 3x3 numerical matrix.
     D = eval(desc)
 
-    # Construct gap matrix Δ(p) = [d(p)⋅σ] jσ2 = [(D p) ⋅ σ] jσ2.
-    # In practice, we do this by calculating Δ = Dᵀ σ jσ2, such
+    # Construct gap matrix Δ(p) = [d(p)⋅σ] jσ2 = [(D'p) ⋅ σ] jσ2.
+    # In practice, we do this by calculating Δ = D'σ jσ2, such
     # that we simply end up with the gap matrix Δ(p) = Δ ⋅ p.
     Δ = np.einsum('kp,kab,bc -> pac', D, σ, jσ2)
 
     # Function for evaluating Δ(p) on the lattice.
     def Δ_p(i: Coord, j: Coord):
-        δ = np.array(j) - np.array(i)
-        if norm(δ) == 1:
-            return np.einsum('iab,i -> ab', Δ, δ)
-        else:
-            return 0 * σ0
+        δ = np.subtract(j, i)
+        return np.einsum('iab,i -> ab', Δ, δ)
 
     return Δ_p
 
@@ -79,48 +73,3 @@ print(Δ_p((0,1,0), (0,0,0)))
 print(Δ_p((0,0,0), (1,0,0)))
 print(Δ_p((0,0,0), (0,1,0)))
 print(Δ_p((0,1,0), (0,0,0)))
-
-with system as (H, Δ, V):
-    for i in lattice.sites():
-        if not (i[0] > 8 and i[0] < Lx-8 and i[1] > 8):
-            H[i, i] = -μ * σ0
-
-    for i, j in lattice.bonds():
-        if not (i[0] > 8 and i[0] < Lx-8 and i[1] > 8) \
-        and not (j[0] > 8 and j[0] < Lx-8 and j[1] > 8):
-            H[i, j] = -t * σ0
-
-tic = time()
-print(system.matrix.tocsr().nnz)
-F = fermi(T)
-print(time() - tic)
-
-with system as (H, Δ, V):
-    for i in lattice.sites():
-        H[i, i] = -μ * σ0
-
-    for i, j in lattice.bonds():
-        H[i, j] = -t * σ0
-
-tic = time()
-print(system.matrix.tocsr().nnz)
-F = fermi(T)
-print(time() - tic)
-
-Lx = 24
-Ly = 24
-
-t = 1
-μ = 0.1
-U = 1.5
-
-T = 1e-6 * t
-
-lattice = CubicLattice((Lx, Ly, 1))
-system = Hamiltonian(lattice)
-fermi = FermiMatrix(system, 2000)
-
-tic = time()
-print(system.matrix.tocsr().nnz)
-F = fermi(T)
-print(time() - tic)
