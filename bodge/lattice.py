66fc0d09680bd52daa7ad649049696b01962c9ee
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-11-14 17:47:16 +0100

    fix: lattice bounds check

--- bodge/lattice.py
+++ bodge/lattice.py
@@ -45,6 +45,8 @@ class Lattice:
             yield (index, index)
         for indices in self.bonds():
             yield indices
+        for indices in self.edges():
+            yield indices
 
     @typecheck
     def index(self, coord: Coord) -> Index:
@@ -61,13 +63,23 @@ class Lattice:
         """Iterate over all atomic bonds in the lattice."""
         raise NotImplementedError
 
+    @typecheck
+    def edges(self) -> Iterator[Coords]:
+        """Iterate over pairs of edges in the lattice."""
+        raise NotImplementedError
+
+
 class CubicLattice(Lattice):
     """Concrete representation of a primitive cubic lattice."""
 
     @typecheck
     def index(self, coord: Coord) -> Index:
         """Convert a 3D site coordinate to a 1D index."""
-        return coord[2] + coord[1] * self.shape[2] + coord[0] * self.shape[1] * self.shape[2]
+        for i in range(3):
+            if coord[i] < 0 or coord[i] >= self.shape[i]:
+                raise ValueError(f"Coordinate {coord} out of bounds")
+        else:
+            return coord[2] + coord[1] * self.shape[2] + coord[0] * self.shape[1] * self.shape[2]
 
     @typecheck
     def sites(self) -> Iterator[Coord]:
@@ -113,7 +125,7 @@ class CubicLattice(Lattice):
         else:
             raise ValueError("No such axis")
 
-    def edges(self, axis: int):
+    def edges(self, axis: Optional[int] = None):
         """Iterate over pairs of atoms at opposite lattice edges.
 
         This is useful if you need periodic boundary conditions. For example,
@@ -121,23 +133,28 @@ class CubicLattice(Lattice):
         `.edges(axis=0)` and define hopping amplitudes between these atoms.
         """
         Lx, Ly, Lz = self.shape[0], self.shape[1], self.shape[2]
-        if axis == 0:
+        if axis is None:
+            # Edges along all axes.
+            yield from self.bonds(axis=2)
+            yield from self.bonds(axis=1)
+            yield from self.bonds(axis=0)
+        elif axis == 0:
             # Edges at x=0 and x=Lx-1.
             for y in range(Ly):
                 for z in range(Lz):
-                    yield (0, y, z), (Lx-1, y, z)
-                    yield (Lx-1, y, z), (0, y, z)
+                    yield (0, y, z), (Lx - 1, y, z)
+                    yield (Lx - 1, y, z), (0, y, z)
         elif axis == 1:
             # Edges at y=0 and y=Ly-1.
             for x in range(Lx):
                 for z in range(Lz):
-                    yield (x, 0, z), (x, Ly-1, z)
-                    yield (x, Ly-1, z), (x, 0, z)
+                    yield (x, 0, z), (x, Ly - 1, z)
+                    yield (x, Ly - 1, z), (x, 0, z)
         elif axis == 2:
             # Edges at z=0 and z=Lz-1.
             for x in range(Lx):
                 for y in range(Ly):
-                    yield (x, y, 0), (x, y, Lz-1)
-                    yield (x, y, Lz-1), (x, y, 0)
+                    yield (x, y, 0), (x, y, Lz - 1)
+                    yield (x, y, Lz - 1), (x, y, 0)
         else:
             raise ValueError("No such axis")
