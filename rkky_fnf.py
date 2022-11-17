58a1d93775edcd304c4fa62b4bb5ada1eb9c258e
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-11-17 07:52:58 +0000

    clean: rkky interactions in f/n/f junctions

--- rkky_fnf.py
+++ rkky_fnf.py
@@ -1,55 +1,58 @@
 #!/usr/bin/env python
 
+"""RKKY interactions between two ferromagnets separated by a normal spacer."""
+
 import numpy as np
 
 from bodge import *
 from bodge.common import *
 from bodge.utils import ldos
 
+# Tight-binding parameters.
 t = 1
-μ = 0.0
+μ = -3
+M = t/2
 
+# Construct a 2D lattice.
+Lx = 100
+Ly = 100
+Lz = 1
+
+lattice = CubicLattice((Lx, Ly, Lz))
+
+# Compare ferromagnetic and antiferromagnetic alignments
+# of the two magnetic layers for various separations δ.
+# Similarly to Deaven et al. (1991), we consider an
+# oblate Fermi surface with t_x < t_y here.
 for δ in range(1, 21):
-    Lx = 20 + 2 * 2
-    Ly = 10
-    Lz = 10
-
-    M = t / 2
-
-    lattice = CubicLattice((Lx, Ly, Lz))
+    # Construct the ferromagnetic Hamiltonian.
     system = Hamiltonian(lattice)
-
     with system as (H, Δ, V):
         for i in lattice.sites():
-            if i[0] == 0 or i[0] == 1:
+            if i[0] >= 0 and i[0] <= 2:
                 H[i, i] = -μ * σ0 + M * σ3
-            elif i[0] == 2 + δ or i[0] == 3 + δ:
+            elif i[0] >= 3 + δ and i[0] <= 5 + δ:
                 H[i, i] = -μ * σ0 + M * σ3
             else:
                 H[i, i] = -μ * σ0
 
         for i, j in lattice.bonds(axis=0):
-            H[i, j] = -0.75*t * σ0
+            H[i, j] = -0.8 * t * σ0
 
         for i, j in lattice.bonds(axis=1):
             H[i, j] = -t * σ0
 
-        for i, j in lattice.bonds(axis=2):
-            H[i, j] = -t * σ0
+    # Calculate the corresponding free energy.
+    E_fm = free_energy(system)
 
-        for i, j in lattice.edges(axis=1):
-            H[i, j] = -t * σ0
-
-        for i, j in lattice.edges(axis=2):
-            H[i, j] = -t * σ0
-
-    F_fm = free_energy(system)
-
+    # Construct the antiferromagnetic Hamiltonian.
     with system as (H, Δ, V):
         for i in lattice.sites():
-            if i[0] == 2 + δ or i[0] == 3 + δ:
+            if i[0] == 3 + δ or i[0] == 4 + δ or i[0] == 5 + δ:
                 H[i, i] = -μ * σ0 - M * σ3
 
-    F_afm = free_energy(system)
+    # Calculate the corresponding free energy.
+    E_afm = free_energy(system)
 
-    print(f"{δ}, {(F_fm - F_afm)/(Ly*Lz)}")
+    # Print the results in CSV format.
+    print(f"{δ}, {(E_fm - E_afm)/(Ly*Lz)}")
