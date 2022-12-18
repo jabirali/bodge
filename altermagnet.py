31d44d9b97c9aad74d8e72c7ed5c83dd876bd016
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-12-18 14:05:10 +0100

    add: altermagnet diag setup

--- altermagnet.py
+++ altermagnet.py
@@ -7,47 +7,74 @@ from tqdm import tqdm
 from bodge import *
 
 # Define lengths of the materials in an SC/NM/AM/NM/SC system.
-L_SC = 20
-L_NM = 10
-L_AM = 20
+L_SC = 10
+L_NM = 3
+L_AM = 10
 
-angle = 45 # or 0
+L_X = 2 * L_SC + 2*L_NM + L_AM
+L_Y = 11
+L_Z = 1
 
-Lx = 2 * L_SC + 2 * L_NM + L_AM
-Ly = 1
-Lz = 1
+lattice = CubicLattice((L_X,L_Y,L_Z))
 
-
-lattice = CubicLattice((Lx, Ly, Lz))
-system = Hamiltonian(lattice)
-fermi = FermiMatrix(system, 1000)
-
+# Parameters corresponding to the lattice model.
 t = 1.0
 h = 0.15 * t
 μ = - t
 Δ0 = - t
 T = 0.01*t
- 
+
+# Parameters used for exploration.
 φs = np.linspace(0.0, 2.0, 100)
 Js = np.zeros_like(φs)
 
-# Instantiate the system.
+# Iterate over phase differences.
 for n, δφ in enumerate( tqdm( φs ) ):
-    with system as (H, Δ, V):
-        for i in lattice.sites():
-            H[i, i] = -μ * σ0
-            if i[0] < L_sc:
-                Δ[i, i] = Δ0 * jσ2 * np.exp((-1j/2) * π * δφ)
-            if i[0] >= Lx - L_sc:
-                Δ[i, i] = Δ0 * jσ2 * np.exp((+1j/2) * π * δφ)
+    # Create the relevant system.
+    system = Hamiltonian(lattice)
+    fermi = FermiMatrix(system, 1000)
 
+    with system as (H, Δ, V):
+        # On-site interactions.
+        for i in lattice.sites():
+            # Every site has same chemical potential.
+            H[i, i] = -μ * σ0
+
+            # Left superconductor.
+            if i[0] + (i[1] - L_Y//2) < L_SC:
+                Δ[i, i] = Δ0 * jσ2 * np.exp((-1j/2) * δφ)
+
+            # Right superconductor.
+            elif i[0] + (i[1] - L_Y//2) >= L_X - L_SC:
+                Δ[i, i] = Δ0 * jσ2 * np.exp((+1j/2) * δφ)
+        
+        # Hopping along x-axis.
+        # TODO: Check hopping b.c.
         for i, j in lattice.bonds(axis=0):
-            H[i, j] = -t * σ0 - h * σ3
+            # Inside altermagnet.
+            if i[0] + (i[1] - L_Y//2) >= (L_SC + L_NM) and i[0] + (i[1] - L_Y//2) < L_X - (L_SC + L_NM):
+                H[i, j] = -t * σ0 - h * σ3
+            elif j[0] + (j[1] - L_Y//2) >= (L_SC + L_NM) and j[0] + (j[1] - L_Y//2) < L_X - (L_SC + L_NM):
+                H[i, j] = -t * σ0 - h * σ3
+            
+            # Outside altermagnet.
+            else:
+                H[i, j] = -t * σ0
+
+        # Hopping along y-axis.
         for i, j in lattice.bonds(axis=1):
-            H[i, j] = -t * σ0 + h * σ3
+            # Inside altermagnet.
+            if i[0] + (i[1] - L_Y//2) >= (L_SC + L_NM) and i[0] + (i[1] - L_Y//2) < L_X - (L_SC + L_NM):
+                H[i, j] = -t * σ0 + h * σ3
+            elif j[0] + (j[1] - L_Y//2) >= (L_SC + L_NM) and j[0] + (j[1] - L_Y//2) < L_X - (L_SC + L_NM):
+                H[i, j] = -t * σ0 + h * σ3
+            
+            # Outside altermagnet.
+            else:
+                H[i, j] = -t * σ0
 
     # Calculate the current.
-    Js[n] = fermi(T).current_elec(axis=0)[(Lx//2, Ly//2, Lz//2)]
+    Js[n] = fermi(T).current_elec(axis=0)[(L_SC + L_NM//2, L_Y//2, L_Z//2)]
 
 plt.plot(φs, Js)
 plt.show()
--- visualize.py
+++ visualize.py
@@ -20,20 +20,23 @@ L_Z = 1
 
 lattice = CubicLattice((L_X,L_Y,L_Z))
 
+# TODO: Refactor into a LabeledLattice class that can assign labels like 'S' and
+# 'N' to sites, and also iterate e.g. `sites(label='S')` and `bonds(labels=('S', 'N'))`.
+
 for i in lattice.sites():
     if i[0] + (i[1] - L_Y//2) < L_SC or i[0] + (i[1] - L_Y//2) >= L_X - L_SC:
         axs[0].scatter(x=i[0], y=i[1], color='#ff7f00')
-    elif i[0] + (i[1] - L_Y//2) < (L_SC + L_NM) or i[0] + (i[1] - L_Y//2) >= L_X - (L_SC + L_NM):
-        axs[0].scatter(x=i[0], y=i[1], color='k')
-    else:
+    elif i[0] + (i[1] - L_Y//2) >= (L_SC + L_NM) and i[0] + (i[1] - L_Y//2) < L_X - (L_SC + L_NM):
         axs[0].scatter(x=i[0], y=i[1], color='#984ea3')
+    else:
+        axs[0].scatter(x=i[0], y=i[1], color='k')
 
 for i in lattice.sites():
     if i[0] < L_SC or i[0] >= L_X - L_SC:
         axs[1].scatter(x=i[0], y=i[1], color='#ff7f00')
-    elif i[0] < L_SC + L_NM or i[0] >= L_X - L_SC - L_NM:
-        axs[1].scatter(x=i[0], y=i[1], color='k')
-    else:
+    elif i[0] >= L_SC + L_NM and i[0] < L_X - L_SC - L_NM:
         axs[1].scatter(x=i[0], y=i[1], color='#984ea3')
+    else:
+        axs[1].scatter(x=i[0], y=i[1], color='k')
 
 plt.show()
