544a27a519481a5f6346d05687ce2816e6dd0634
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2022-12-20 15:13:19 +0000

    fix: bug in current calculation

--- bodge/fermi.py
+++ bodge/fermi.py
@@ -103,6 +103,9 @@ class FermiMatrix:
         Ω = self.scale
         J = np.zeros(self.lattice.shape, dtype=np.float64)
         for i, j in self.lattice.bonds(axis):
-            J[i] = (Ω / 2) * np.imag(np.trace(self.hopp[i, j] - self.hopp[j, i]))
+            try:
+                J[i] += (Ω / 2) * (j[axis] - i[axis]) * np.imag(np.trace(self.hopp[i, j] - self.hopp[j, i]))
+            except KeyError:
+                pass
 
         return J
