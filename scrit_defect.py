9a0cfdab7e09b9f87fb7c9f1108d63903a038869
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2023-05-29 14:01:34 +0200

    add: magnetic texture in superconducting critical temperature

--- scrit_defect.py
+++ scrit_defect.py
@@ -47,7 +47,7 @@ def main(delta: str):
             return NM(i) or SC(i)
 
         # Calculate critical temperature.
-        for τ in [0.05]:  # np.arange(0, 0.1, 0.01):
+        for τ in [0.03]:  # np.arange(0, 0.1, 0.01):
             system = Hamiltonian(lattice)
             with system as (H, Δ, V):
                 for i in lattice.sites():
@@ -73,7 +73,7 @@ def main(delta: str):
                     if IN(i) and IN(j):
                         H[i, j] = -τ * σ0
 
-            Tc = critical_temperature(system, T_max=0.02)
+            Tc = critical_temperature(system, T_max=0.04)
 
             f.write(f"{sec}, {Tc}\n")
             f.flush()
