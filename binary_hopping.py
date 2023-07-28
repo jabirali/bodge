5a0afcfc703c55bfa2380ed80c0502e8668118fc
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2023-07-28 17:19:52 +0200

    add: better logfile

--- binary_hopping.py
+++ binary_hopping.py
@@ -29,20 +29,61 @@ def main(tau: float, delta: str):
     def IN(i: Coord):
         return NM(i) or SC(i)
 
-    # Load the interpolated profiles.
+    # Load the interpolated magnetization profiles.
     filename = f"m_{delta}.npz"
     log.debug(f"Filename: {filename}")
     with np.load(filename) as f:
         mx, my, mz = f["mx"], f["my"], f["mz"]
 
+    # Generate spin matrices from the magnetization profiles.
+    def σ(i):
+        x, y = i[:-1]
+        return mx[x, y] * σ1 + my[x, y] * σ2 + mz[x, y] * σ3
+
+    # Model parameters.
+    t = 1.0
+    μ = 0.5
+    m = 0.3
+    U = t
+    τ = tau
+    log.info(f"Parameters: {t}, {τ}, {μ}, {m}, {U}")
+
+    log.debug(f"Constructing the base Hamiltonian")
+    system = Hamiltonian(lattice)
+    with system as (H, Δ, V):
+        for i in lattice.sites():
+            # Chemical potential in non-empty space,
+            # exchange field in non-superconductors.
+            # Attractive Hubbard in superconductors.
+            if NM(i):
+                H[i, i] = -μ * σ0 - m * σ(i)
+            if SC(i):
+                H[i, i] = -μ * σ0
+                V[i, i] = -U
+
+        # Intra-plane hopping coefficient t.
+        for i, j in lattice.bonds(axis=0):
+            if IN(i) and IN(j):
+                H[i, j] = -t * σ0
+        for i, j in lattice.bonds(axis=1):
+            if IN(i) and IN(j):
+                H[i, j] = -t * σ0
+
+    # Inter-plane hopping coefficient τ.
+    log.debug(f"Updating inter-layer hopping term")
+    with system as (H, Δ, V):
+        for i, j in lattice.bonds(axis=2):
+            if IN(i) and IN(j):
+                H[i, j] = -τ * σ0
+
 
 if __name__ == "__main__":
     log.basicConfig(
-        filename="main.log",
+        filename=__file__.replace(".py", ".log"),
         filemode="w",
-        format="%(asctime)s %(levelname)s \t%(message)s",
+        format="\x1b[32m%(asctime)s \x1b[33m%(levelname)s \x1b[0m%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         level=log.DEBUG,
     )
-    log.info(f"{' '.join(sys.argv)}")
+    log.info(f"Executing: {sys.argv}")
     typer.run(main)
