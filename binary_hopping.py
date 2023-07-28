935b3fa035f1d7f219848fa75f304022e5b6e8ba
Jabir Ali Ouassou <jabir.a.ouassou@ntnu.no>
2023-07-28 18:36:23 +0200

    feat: finished implementing the binary hopping script

--- binary_hopping.py
+++ binary_hopping.py
@@ -7,11 +7,12 @@ import sys
 
 import numpy as np
 import typer
+from tqdm import trange
 
 from bodge import *
 
 
-def main(tau: float, delta: str):
+def main(delta: str):
     # Construct an appropriate lattice, including functions to determine
     # whether a particular region is superconducting or normal metallic.
     Lx, Ly, Lz = 64, 64, 2
@@ -42,11 +43,9 @@ def main(tau: float, delta: str):
 
     # Model parameters.
     t = 1.0
-    μ = 0.5
-    m = 0.3
-    U = t
-    τ = tau
-    log.info(f"Parameters: {t}, {τ}, {μ}, {m}, {U}")
+    μ = 0.5 * t
+    m = 0.3 * t
+    U = 1.0 * t
 
     log.debug(f"Constructing the base Hamiltonian")
     system = Hamiltonian(lattice)
@@ -69,21 +68,53 @@ def main(tau: float, delta: str):
             if IN(i) and IN(j):
                 H[i, j] = -t * σ0
 
-    # Inter-plane hopping coefficient τ.
-    log.debug(f"Updating inter-layer hopping term")
-    with system as (H, Δ, V):
-        for i, j in lattice.bonds(axis=2):
-            if IN(i) and IN(j):
-                H[i, j] = -τ * σ0
+    log.debug(f"Constructing the Fermi matrix")
+    fermi = FermiMatrix(system, order=1200)
+
+    # Binary search procedure.
+    T_c = 0.01 * t
+    τ_min = 0.0 * t
+    τ_max = 1.0 * t
+    Δ_init = 1e-4
+
+    for n in trange(20, unit="τ", smoothing=0):
+        # Update current hopping parameter.
+        τ_now = (τ_min + τ_max) / 2
+        log.info(f"Inter-layer hopping: {τ_now}")
+        with system as (H, Δ, V):
+            for i, j in lattice.bonds(axis=2):
+                if IN(i) and IN(j):
+                    H[i, j] = -τ_now * σ0
+
+        # Self-consistency iterations.
+        Δ_now = {i: Δ_init for i in lattice.sites()}
+        for m in trange(8, unit="Δ", smoothing=0):
+            with system as (H, Δ, V):
+                for i in lattice.sites():
+                    if (i, i) in V:
+                        Δ[i, i] = Δ_now[i] * jσ2
+            Δ_now = fermi(T_c).order_swave()
+            log.debug(f"Mean gap: {np.mean(np.abs(Δ_now))}")
+
+        # Update hopping interval based on gap change.
+        Δ_fin = np.median(np.abs(Δ_now[np.nonzero(Δ_now)]))
+        if Δ_fin > Δ_init:
+            τ_min = τ_now
+        else:
+            τ_max = τ_now
+
+    τ_now = (τ_min + τ_max) / 2
+    log.info(f"Obtained the final value: τ = {τ_now} t.")
 
 
 if __name__ == "__main__":
     log.basicConfig(
         filename=__file__.replace(".py", ".log"),
-        filemode="w",
         format="\x1b[32m%(asctime)s \x1b[33m%(levelname)s \x1b[0m%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         level=log.DEBUG,
     )
-    log.info(f"Executing: {sys.argv}")
+
+    args = " ".join(sys.argv)
+    log.info(f"\x1b[31;1m{args}\x1b[0m")
     typer.run(main)
