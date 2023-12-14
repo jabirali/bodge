018b4751dd73330b28fe45542f2231d0b2f2199c
Jabir Ali Ouassou <jabir.ali.ouassou@hvl.no>
2023-12-14 15:53:32 +0100

    clean: remove the chebyshev expansion from the main branch

---
+++ tests/test_common.py
@@ -0,0 +1,19 @@
+import pytest
+from scipy.stats import unitary_group
+
+from bodge.chebyshev import *
+from bodge.common import *
+
+
+def test_pauli():
+    # Test that the quaternion identities hold.
+    assert np.allclose(σ1 @ σ1, σ0)
+    assert np.allclose(σ2 @ σ2, σ0)
+    assert np.allclose(σ3 @ σ3, σ0)
+
+    assert np.allclose(σ1 @ σ2, jσ3)
+    assert np.allclose(σ2 @ σ3, jσ1)
+    assert np.allclose(σ3 @ σ1, jσ2)
+
+    assert np.allclose(σ1 @ σ2 @ σ3, jσ0)
+
