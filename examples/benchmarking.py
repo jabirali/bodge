2c743a05ea34f41fbd3f284b8ff80979d3a56740
Jabir Ali Ouassou <jabir.ali.ouassou@hvl.no>
2023-12-15 17:00:36 +0100

    clean: bodge vs. kwant benchmark script

--- examples/benchmarking.py
+++ examples/benchmarking.py
@@ -1,114 +1,134 @@
 #!/usr/bin/env python
 
-"""Benchmarking of sparse matrix construction for a BdG Hamiltonian in Bodge vs. Kwant."""
-
-# ------------------------------------------------------------
-# Implementation in Kwant
-# ------------------------------------------------------------
-
-# ------------------------------------------------------------
-# Implementation in Kwant
-# ------------------------------------------------------------
-
-from time import time
+"""Benchmarking of Bodge vs. Kwant when constructing a Bogoliubov-deGennes Hamiltonian."""
 
 import kwant
 import numpy as np
-
-# # For plotting
-# from matplotlib import pyplot
-
-t = 1
-
-L = 200
-W = 200
-
-μ = -3 * t
-Δ0 = 0.1 * t
-M0 = 1.5 * t
-
-σ_0 = np.array([[1, 0], [0, 1]])
-σ_1 = np.array([[0, 1], [1, 0]])
-σ_2 = np.array([[0, -1j], [1j, 0]])
-σ_3 = np.array([[1, 0], [0, -1]])
-
-τ_0 = σ_0.copy()
-τ_1 = σ_1.copy()
-τ_2 = σ_2.copy()
-τ_3 = σ_3.copy()
-
-t = time()
-
-# Construct an LxW square lattice.
-lattice = kwant.lattice.square(norbs=4)
-system = kwant.Builder()
-
-# On-site potentials.
-for x in range(L):
-    for y in range(W):
-        if x < L // 2:
-            # Superconducting region.
-            system[lattice(x, y)] = -μ * np.kron(τ_3, σ_0) - Δ0 * np.kron(1j * τ_2, 1j * σ_2)
-        else:
-            # Ferromagnetic region.
-            system[lattice(x, y)] = -μ * np.kron(τ_3, σ_0) - M0 * np.kron(τ_3, σ_3)
-
-# Hopping terms.
-system[lattice.neighbors()] = -t * np.kron(τ_3, σ_0)
-
-# Finalize the Hamiltonian.
-system = system.finalized()
-
-t = time() - t
-print(f"Kwant object construction: {t}")
-
-# Generate a sparse matrix.
-t = time()
-H_kwant_sparse = system.hamiltonian_submatrix(sparse=True)
-t = time() - t
-print(f"Kwant sparse construction: {t}")
-
-# Generate a dense matrix.
-t = time()
-# H_kwant_dense = system.hamiltonian_submatrix(sparse=False)
-t = time() - t
-print(f"Kwant dense construction: {t}")
-
-# Create a comparable Bodge setup.
 from bodge import *
 
-t = time()
+t = 1  # Hopping parameter
+μ = -3 * t  # Chemical potential
+M0 = 1.5 * t  # Magnetic exchange field
+Δ0 = 0.1 * t  # Superconducting gap
+χ = 0.1  # Superconducting phase winding
 
-lattice = CubicLattice((L, W, 1))
-system = Hamiltonian(lattice)
 
-with system as (H, Δ):
-    for i in lattice.sites():
-        if i[0] < L // 2:
-            # Superconducting region.
-            H[i, i] = -μ * σ0
-            Δ[i, i] = -Δ0 * jσ2
-        else:
-            # Ferromagnetic region.
-            H[i, i] = -μ * σ0 - M0 * σ3
-    for i, j in lattice.bonds(axis=0):
-        H[i, j] = -t * σ0
-        # Δ[i, j] = Δ0 * jσ2
-    for i, j in lattice.bonds(axis=1):
-        H[i, j] = -t * σ0
+def timer(timers=[]):
+    """Simple timer used for benchmarking."""
+    from time import time
 
-t = time() - t
-print(f"Bodge object creation: {t}")
+    # Save the current timestamp.
+    timers.append(time())
 
-t = time()
-H_bodge_sparse = system(format="csr")
-t = time() - t
-print(f"Bodge sparse construction: {t}")
+    # Return the difference since last time stamp.
+    try:
+        return timers[-1] - timers[-2]
+    except:
+        return None
 
-t = time()
-# H_bodge_dense = system(format="dense")
-t = time() - t
-print(f"Bodge dense construction: {t}")
 
-print(np.max(np.abs(H_bodge_sparse - H_kwant_sparse)))
-print(np.max(np.abs(H_bodge_dense - H_kwant_dense)))
+def bench_kwant(L, W, sparse=True):
+    """Construct a Bogoliubov-deGennes Hamiltonian on a square lattice using Kwant."""
+    # Import the required libraries.
+
+    # Define Pauli matrices for spin degree of freedom.
+    σ_0 = np.array([[1, 0], [0, 1]])
+    σ_1 = np.array([[0, 1], [1, 0]])
+    σ_2 = np.array([[0, -1j], [1j, 0]])
+    σ_3 = np.array([[1, 0], [0, -1]])
+
+    # Define Pauli matrices for particle-hole degree of freedom.
+    τ_0 = σ_0.copy()
+    τ_1 = σ_1.copy()
+    τ_2 = σ_2.copy()
+    τ_3 = σ_3.copy()
+
+    # Start a timer.
+    timer()
+
+    # Construct a square lattice and the Hamiltonian.
+    lattice = kwant.lattice.square(norbs=4)
+    system = kwant.Builder()
+
+    # Fill out the terms in the Hamiltonian.
+    for x in range(L):
+        # Calculate the complex phase φ = πx/L of the order parameter at this point.
+        φ = np.pi * x / L
+
+        # The particle-hole structure of the pairing term depends on this phase.
+        τ_φ = 1j * (τ_2 * np.cos(φ) + τ_1 * np.sin(φ))
+
+        # Fill out the on-site contributions of the Hamiltonian.
+        for y in range(W):
+            if x < L // 2:
+                # Superconducting region.
+                system[lattice(x, y)] = -μ * np.kron(τ_3, σ_0) - Δ0 * np.kron(τ_φ, 1j * σ_2)
+            else:
+                # Ferromagnetic region.
+                system[lattice(x, y)] = -μ * np.kron(τ_3, σ_0) - M0 * np.kron(τ_3, σ_3)
+
+    # Hopping terms.
+    for x in range(L - 1):
+        for y in range(W):
+            system[lattice(x, y), lattice(x + 1, y)] = -t * np.kron(τ_3, σ_0)
+    for x in range(L):
+        for y in range(W - 1):
+            system[lattice(x, y), lattice(x, y + 1)] = -2 * t * np.kron(τ_3, σ_0)
+
+    # Compile the Hamiltonian object to a matrix.
+    system = system.finalized()
+    H = system.hamiltonian_submatrix(sparse=sparse)
+
+    # Report the benchmark results.
+    print(f"Kwant time for {L}x{W} lattice: {timer()} seconds")
+
+    return H
+
+
+def bench_bodge(L, W, sparse=True):
+    """Construct a Bogoliubov-deGennes Hamiltonian on a square lattice using Bodge."""
+    # Start a timer.
+    timer()
+
+    # Construct the square lattice and the Hamiltonian.
+    lattice = CubicLattice((L, W, 1))
+    system = Hamiltonian(lattice)
+
+    # Fill out the terms in the Hamiltonian.
+    with system as (H, Δ):
+        # On-site terms.
+        for i in lattice.sites():
+            if i[0] < L // 2:
+                # Superconducting region.
+                H[i, i] = -μ * σ0
+                Δ[i, i] = -Δ0 * np.exp(1j * π * i[0] / L) * jσ2
+            else:
+                # Ferromagnetic region.
+                H[i, i] = -μ * σ0 - M0 * σ3
+
+        # Hopping terms.
+        for i, j in lattice.bonds(axis=0):
+            H[i, j] = -t * σ0
+        for i, j in lattice.bonds(axis=1):
+            H[i, j] = -2 * t * σ0
+
+    # Compile the Hamiltonian object to a matrix.
+    if sparse:
+        H = system(format="csr")
+    else:
+        H = system(format="dense")
+
+    # Report the benchmark results.
+    print(f"Bodge time for {L}x{W} lattice: {timer()} seconds")
+
+    return H
+
+
+if __name__ == "__main__":
+    for N in [10, 30, 100, 300]:
+        # Benchmark the matrix construction.
+        H1 = bench_kwant(N, N)
+        H2 = bench_bodge(N, N)
+
+        # Ensure that the results are equal.
+        print(f"Difference: {np.max(np.abs(H1 - H2))}")
