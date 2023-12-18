f39b0f7d8ca54ac39b4e4ece6b9afdac6f9946a7
Jabir Ouassou <jabirali@login-3.saga>
2023-12-18 17:22:36 +0100

    add: backup of throwaway scripts

---
+++ scrit_defect_quick.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+"""Calculation of critical temperature for s-wave superconductors with a magnetic defect."""
+
+import numpy as np
+from icecream import ic
+from typer import run
+
+from bodge import *
+
+def main(delta: str, tau: float):
+    ic(delta)
+    ic(tau)
+
+    # Construct an appropriate lattice, including functions to determine
+    # whether a particular region is superconducting or normal metallic.
+    Lx, Ly, Lz = 64, 64, 2
+    lattice = CubicLattice((Lx, Ly, Lz))
+
+    def NM(i: Coord):
+        x, y, z = i
+        return z == 1
+
+    def SC(i: Coord):
+        x, y, z = i
+        return z == 0 and x >= Lx // 4 and x < 3 * Lx // 4 and y >= Ly // 4 and y < 3 * Ly // 4
+
+    def IN(i: Coord):
+        return NM(i) or SC(i)
+
+    # Load the interpolated profiles.
+    δ = delta
+    with np.load(f"m_{δ}.npz") as f:
+        mx, my, mz = f["mx"], f["my"], f["mz"]
+        ic(mx, my, mz)
+    
+    # Define a function for magnetization at coordinates.
+    def σ(i):
+        x, y = i[:-1]
+        return mx[x, y] * σ1 + my[x, y] * σ2 + mz[x, y] * σ3
+    
+    # Perform Tc calculations.
+    with open(f"scrit_weaker_m_{δ}_{tau}.dat", "w") as f:
+        # Model parameters.
+        t = 1.0
+        μ = 0.5
+        m = 0.3
+        U = t
+        τ = tau
+
+        # Calculate critical temperature.
+        system = Hamiltonian(lattice)
+        with system as (H, Δ, V):
+            for i in lattice.sites():
+                # Chemical potential in non-empty space,
+                # exchange field in non-superconductors.
+                # Attractive Hubbard in superconductors.
+                if NM(i):
+                    H[i, i] = -μ * σ0 - m * σ(i)
+                if SC(i):
+                    H[i, i] = -μ * σ0
+                    V[i, i] = -U
+
+            # Intra-plane hopping coefficient t.
+            for i, j in lattice.bonds(axis=0):
+                if IN(i) and IN(j):
+                    H[i, j] = -t * σ0
+            for i, j in lattice.bonds(axis=1):
+                if IN(i) and IN(j):
+                    H[i, j] = -t * σ0
+
+            # Inter-plane hopping coefficient τ.
+            for i, j in lattice.bonds(axis=2):
+                if IN(i) and IN(j):
+                    H[i, j] = -τ * σ0
+
+        Tc = critical_temperature(system, T_max=0.04, bisects=6, iters=4)
+
+        f.write(f"{δ}, {τ}, {Tc}\n")
+        f.flush()
+
+if __name__ == "__main__":
+    ic()
+    run(main)
+    ic()
---
+++ ysr_imp_3.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+"""Calculate the local density of states around a magnetic impurity.
+
+This is useful to e.g. determine the YSR bound states that might exist
+in such materials, which is likely related to RKKY oscillations.
+"""
+
+# %% Imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from bodge import *
+from bodge.utils import ldos, pwave
+from scipy.signal import find_peaks
+
+# %% Parameters used in paper.
+t = 1.0
+μ = -3.0 * t
+Δ0 = 0.1 * t
+J0 = 3.0 * t
+
+# %% Construct a 2D lattice.
+# Lx = 200
+# Ly = 200
+Lx = 800
+Ly = 800
+Lz = 1
+print(Lx, Ly, Lz)
+
+lattice = CubicLattice((Lx, Ly, 1))
+
+# %% Function to perform calculations.
+def main(ds, δs):
+    dfs = []
+    for d in ds:
+        # Convert string to d-vector.
+        d_ = pwave(d)
+
+        # Construct the base Hamiltonian.
+        system = Hamiltonian(lattice)
+        with system as (H, Δ, _):
+            for i in lattice.sites():
+                H[i, i] = -μ * σ0
+            for i, j in lattice.bonds():
+                H[i, j] = -t * σ0
+                Δ[i, j] = -Δ0 * d_(i, j)
+
+        # Prepare calculation points.
+        energies = np.linspace(0.00, 2 * Δ0, 51)
+        i = (Lx // 2, Ly // 2, 0)
+        sites = [(i[0] + δ, i[1], i[2]) for δ in δs]
+
+        # Calculate the density of states without impurity.
+        df0 = ldos(system, sites, energies)
+        df0['d'] = d
+        df0['imp'] = False
+
+        print(df0)
+        dfs.append(df0)
+
+        # Add the impurities to the Hamiltonian.
+        with system as (H, Δ, V):
+            H[i, i] = -μ * σ0 - (J0/2) * σ3
+
+        # Calculate the density of states with impurity.
+        df1 = ldos(system, sites, energies)
+        df1['d'] = d
+        df1['imp'] = True
+
+        print(df1)
+        dfs.append(df1)
+
+        df = pd.concat(dfs)
+        save(df)
+
+    return df
+
+def save(df, name='ysr3.csv'):
+    df['δ'] = df['x'] - Lx//2
+    df.to_csv(name, columns=['d','δ','imp','ε','dos'], index=False)
+
+# %% Test the function above.
+# ds = ["e_x * p_x"]
+# δs = [1, 2]
+# df = main(ds, δs)
+# # display(df)
+# save(df, 'test.csv')
+
+# %% Perform actual calculations.
+ds = ["e_z * (p_x + jp_y)"]
+δs = [1, 2, 3, 4, 5]
+df = main(ds, δs)
+#save(df)
